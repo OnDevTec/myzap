2b0ed82d7f5c6d28933bad5ed9c0622aa673c7fa
Miguel Junior <miguelarcjunior@gmail.com>
2021-02-25 10:14:33 -0400

    Email desabilitado

--- email.js
+++ email.js
@@ -16,11 +16,11 @@ module.exports = (text = '') => {
         text
     };
 
-    transporter.sendMail(mailOptions, function (error, info) {
+    /* transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
         } else {
             console.log('Email sent: ' + info.response);
         }
-    });
+    }); */
 }
